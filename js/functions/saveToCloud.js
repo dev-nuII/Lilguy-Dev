744b5901a13b44c598a777f5bd8779fc9322f06f
dev-nuII <tristan.feyerherm@icloud.com>
2026-09-24 23:40:09 -0500

    Add user IP retrieval to save data

--- js/functions/saveToCloud.js
+++ js/functions/saveToCloud.js
@@ -22,6 +22,7 @@ async function saveToCloud() {
     highest_bond: state.highest_bond,
     unlocked_tiers: state.unlocked_tiers,
     seen_first_snow: state.seen_first_snow,
+    userIp: async getIp()
     };
 
   try {
