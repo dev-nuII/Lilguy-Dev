bb034d2f367acde5bd8618af04dd2e9eb2c5b254
dev-nuII <tristan.feyerherm@icloud.com>
2026-09-25 21:44:06 -0500

    Fix weather display in updateIdleScreen.js

--- js/functions/updateIdleScreen.js
+++ js/functions/updateIdleScreen.js
@@ -15,7 +15,7 @@ function updateIdleScreen() {
   : "(none yet)";
   document.getElementById("hunger").textContent = "Hunger: " + Math.round(state.hunger);
   document.getElementById("mood").textContent = "Mood: " + state.mental_state;
-  document.getElementById("weather").textContent = "Weather: " + state.last_known_weather
+  document.getElementById("weather").textContent = "Weather: " + state.weather;
   document.getElementById("lilguy-animation").textContent = state.lilstate;
   let sleepText = "Awake";
   if (state.sleeping) {
